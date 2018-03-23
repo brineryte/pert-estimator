e9a5ee22b8d08e300ae0c97e681f4b6801ba7958
Brian Wright <bwright@webstaurantstore.com>
2018-03-23 16:59:14 -0400

    Finished JS formula logic for caltulation. Final touches.

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
-let elements = document.getElementsByTagName('input');
-console.log(elements);
+// get elements
+const bestInput = document.getElementById("bestInput");
+const likelyInput = document.getElementById("likelyInput");
+const worstInput = document.getElementById("worstInput");
+const metricSelect = document.getElementsByClassName("metricSelect");
+let currentMetric = metricSelect[0];
 
-function addEvent(element, type, handler){
-    if(element.attachEvent){
-        element.attachEvent('on' + type, handler);
-    } else {
-        element.addEventListener(type, handler);
-    }
+const O = document.getElementsByClassName("O");
+const N = document.getElementsByClassName("N");
+const P = document.getElementsByClassName("P");
+
+const estimateOut = document.getElementsByClassName("estimate");
+const deviationOut = document.getElementsByClassName("deviation");
+const metricOut = document.getElementsByClassName("metricOut");
+
+//default the metric
+[].forEach.call( metricOut, el => el.innerHTML = currentMetric.value );
+
+addEvent(bestInput, "keyup");
+addEvent(likelyInput, "keyup");
+addEvent(worstInput, "keyup");
+addEvent(metricSelect[0], "change");
+
+function addEvent(element, event){
+    element.addEventListener(event, 
+                            () => 
+                            { 
+                                updateDisplay();
+                            });
 }
 
-for(var i = 0; i < elements.length; i++) {
-    addEvent(elements[i], 'change', () => console.log(elements[i].value));
+function updateDisplay() {
+    let metric = metricSelect[0].value;
+    let opt = bestInput.value != '' ? bestInput.value : 0;
+    let nom = likelyInput.value != '' ? likelyInput.value : 0;
+    let pes = worstInput.value != '' ? worstInput.value : 0;
+    let estimate = (( 4*nom + 1*opt + 1*pes )/6).toFixed(1);
+    let deviation = ((1*pes - 1*opt)/6).toFixed(1);
+
+    [].forEach.call(O, el => el.innerHTML = opt);
+    [].forEach.call(N, el => el.innerHTML = nom);
+    [].forEach.call(P, el => el.innerHTML = pes);
+    [].forEach.call(estimateOut, el => el.innerHTML = estimate);
+    [].forEach.call(deviationOut, el => el.innerHTML = deviation);
+    [].forEach.call(metricOut, el => el.innerHTML = metric);
 }
